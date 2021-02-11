5189ffb9a93e7e081058c53a69116340b79b8224
Paulo Salvatore <salvatoregames@gmail.com>
2021-02-11 14:16:41 -0300

    PisoEspecial desativando Pedra quando a Caixa está em cima

---
+++ Assets/PisoEspecial.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PisoEspecial : MonoBehaviour
+{
+    public GameObject alvo;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Caixa"))
+        {
+            alvo.SetActive(false);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Caixa"))
+        {
+            alvo.SetActive(true);
+        }
+    }
+}
